66801180058f0e7a8cfd2ba1081d680fb750bc9b
zyvas <372269470@qq.com>
2014-12-21 19:36:00 +0800

    完成爬虫任务

--- download/create-db.py
+++ download/create-db.py
@@ -8,7 +8,7 @@ def create():
         conn = MySQLdb.connect(host='localhost',user='root',passwd='123456',charset='utf8')
         cur = conn.cursor()
         
-        cur.execute('drop database dyttmovie')
+        cur.execute('drop database if exists dyttmovie')
         
         cur.execute("create database if not exists dyttmovie charset='utf8'")
         conn.select_db('dyttmovie')
@@ -18,14 +18,18 @@ def create():
             id int ,
             title varchar(100) not null,
             publishdate varchar(50),
-            imageurl varchar(100),
+            imageurl varchar(500),
+            type varchar(100),
+            language varchar(100),
             subtitles varchar(30),
             fileformat varchar(30),
             width varchar(20),
             height varchar(20),
             size varchar(100),
-            duration varchar(50),
-            director varchar(50)
+            duration varchar(100),
+            director varchar(100),
+            introduceimageurl varchar(100),
+            downloadlink varchar(200)
             )''')
 
         # chname
@@ -41,37 +45,17 @@ def create():
         #name
         cur.execute('''create table names(
             id int,
-            name varchar(100)
+            name varchar(500)
             )''')
         cur.execute('''create table movie_names(
             movie_id int,
             name_id int
             )''')
 
-        #type
-        cur.execute('''create table types(
-            id int,
-            type varchar(20)
-            )''')
-        cur.execute('''create table movie_types(
-            movie_id int,
-            type_id int
-            )''')
-
-        #language
-        cur.execute('''create table languages(
-            id int,
-            language varchar(20)
-            )''')
-        cur.execute('''create table movie_languages(
-            movie_id int,
-            language_id int
-            )''')
-
         #actors
         cur.execute('''create table actors(
             id int,
-            actor varchar(50)
+            actor varchar(500)
             )''')
         cur.execute('''create table movie_actors(
             movie_id int,
@@ -81,32 +65,13 @@ def create():
         #introduce
         cur.execute('''create table introduces(
             id int,
-            introduce varchar(10000)
+            introduce varchar(4000)
             )''')
         cur.execute('''create table movie_introduces(
             movie_id int,
             introduce_id int
             )''')
-        
-        #introduceimageurl
-        cur.execute('''create table iiurls(
-            id int,
-            iirul varchar(100)
-            )''')
-        cur.execute('''create table movie_iiurls(
-            movie_id int,
-            iiurl_id int
-            )''')
 
-        #downloadurl
-        cur.execute('''create table durls(
-            id int,
-            durl varchar(100)
-            )''')
-        cur.execute('''create table movie_durls(
-            movie_id int,
-            durl_id int
-            )''')
         cur.close()
     except MySQLdb.Error,e:
         print "Mysql Error %d : %s"%(e.args[0],e.args[1])
